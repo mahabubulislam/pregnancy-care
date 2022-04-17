a88ee7875b14764bfd897f7d763efbe48e069876
mahbub-webdev <mahabub2k1@gmail.com>
2022-04-18 00:36:44 +0600

    things style added

--- src/components/Things/Things.js
+++ src/components/Things/Things.js
@@ -3,16 +3,10 @@ import React from 'react';
 const Things = ({ things }) => {
     const { name, img } = things
     return (
-        <div className='border-2 even:border-rose-500 rounded-lg odd:border-blue-600'>
-            <div
-                className='h-80 rounded-lg bg-cover bg-no-repeat relative'
-                style={{
-                    backgroundImage: `url(${img})`
-                }}>
-                <div className=' bottom-0 absolute bg-slate-300/60 p-12 w-full rounded-t-[70px] '>
-                    <h3 className='text-4xl'>{name}</h3>
-                </div>
-
+        <div className='border-2 even:border-rose-500 rounded-lg odd:border-blue-600 cursor-pointer'>
+            <div className="text-center relative">
+                <img className='w-full h-72' src={img} alt='...'/>
+                <h1 className='absolute bottom-0 left-auto text-2xl bg-slate-200/70 w-full p-10 font-bold rounded-t-3xl hover:scale-105 ease-in duration-500'>{name}</h1>
             </div>
         </div>
     );
--- src/components/ThingsToKnow/ThingsToKnow.js
+++ src/components/ThingsToKnow/ThingsToKnow.js
@@ -10,7 +10,7 @@ const ThingsToKnow = () => {
     }, [])
     return (
         <div className='my-10 w-4/5 mx-auto'>
-            <h1 className='text-3xl text-center text-rose-500'>Things To Know</h1>
+            <h1 className='text-3xl text-center text-rose-500 my-5'>Things To Know</h1>
             <div className='grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 my-2'>
                 {
                     things.map(thing => <Things
