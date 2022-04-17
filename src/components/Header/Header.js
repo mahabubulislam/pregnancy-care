3925a46d352c31b58379f194cb7a5ec7239c1a88
mahbub-webdev <mahabub2k1@gmail.com>
2022-04-17 15:20:32 +0600

    signout added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 import logo from '../../images/logo/pregnant.png'
 import { MenuIcon, XIcon } from '@heroicons/react/solid'
 import CustomLink from '../CustomLink/CustomLink';
-
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
+import { useNavigate } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
 const Header = () => {
     const [open, setOpen] = useState(false)
+    const [user, loading, error] = useAuthState(auth);
+    const navigate = useNavigate()
     return (
 
 
@@ -21,10 +26,17 @@ const Header = () => {
                 </div>
                 <ul className={`lg:flex lg:items-center lg:pb-0 pb-12 absolute lg:static bg-white lg:z-auto z-[-1] left-0 w-full lg:w-auto mt-5 lg:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
                     <CustomLink to='/home'>Home</CustomLink>
-                    <CustomLink to='/procedures'>Procedures</CustomLink>
                     <CustomLink to='/appoinment'>Appoinment</CustomLink>
                     <CustomLink to='/blogs'>Blogs</CustomLink>
-                    <CustomLink to='/login'>Login</CustomLink>
+                    <CustomLink to='/about'>About</CustomLink>
+                    {
+                        user?
+                        <button onClick={()=>signOut(auth)} className='bg-rose-500 p-2 md:ml-8 text-xl md:my-0 my-4 py-1 text-white rounded-lg'>Logout</button>
+                        :
+                        <button onClick={()=>navigate('/login')}  className='bg-rose-500 p-2 md:ml-8 text-xl md:my-0 my-4 py-1 text-white rounded-lg'>Login</button>
+                        
+                        
+                    }
                 </ul>
             </div>
         </nav>
